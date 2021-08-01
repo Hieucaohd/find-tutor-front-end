9745a711114fa0b345ae3528d3bf7bdb186c2762
hiepnguyen223 <hiepnguyenn223@gmail.com>
2021-08-01 12:32:52 +0700

    register bug

--- src/feature/register/registerAccount.js
+++ src/feature/register/registerAccount.js
@@ -26,17 +26,7 @@ export const registerInfo = async ( args ) => {
           method: "POST",
           header: args.token,
           body: {
-            profession: args.profession,
-            experience: args.experience,
-            achievement: args.achievement,  
-            cap_day: null,
-            lop_day: null,
-            number_phone: args.telephone,
-            number_of_identity_card: args.identitycard,
-            first_name: null,
-            last_name: args.name,
-            birthday: args.birthday,
-            location: null,
+            
           }
         }
       ).then((response) => {
