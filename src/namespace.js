2f7cb3553a24341888e24b27d154afcb4de2ce4d
hieucaohd <hieucaohd@gmail.com>
2021-10-15 09:35:41 +0700

    thay doi server websocket

--- src/namespace.js
+++ src/namespace.js
@@ -4,5 +4,5 @@ export const token_prefix = 'Bearer';
 export const endpoint = 'http://ec2-18-141-198-53.ap-southeast-1.compute.amazonaws.com:8000/graphql/';
 export const server_name_chat_app = 'https://tim-gia-su-chat.herokuapp.com';
 export const endpoint_chat_app = 'https://tim-gia-su-chat.herokuapp.com/graphql';
-export const notification_socket = "wss://tim-gia-su.herokuapp.com/ws/notification/";
-export const room_socket = "wss://tim-gia-su.herokuapp.com/ws/room/";
+export const notification_socket = "ws://ec2-18-141-198-53.ap-southeast-1.compute.amazonaws.com:8000/ws/notification/";
+export const room_socket = "ws://ec2-18-141-198-53.ap-southeast-1.compute.amazonaws.com:8000/ws/room/";
