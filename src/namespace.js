eac157c4075c9687afe6014a75d68afa3505c6f0
hieucaohd <hieucaohd@gmail.com>
2021-10-15 08:58:17 +0700

    thay doi server sang aws

--- src/namespace.js
+++ src/namespace.js
@@ -1,7 +1,7 @@
-export const server_name = 'https://tim-gia-su.herokuapp.com';
+export const server_name = 'http://ec2-18-141-198-53.ap-southeast-1.compute.amazonaws.com:8000';
 export const province_server = '';
 export const token_prefix = 'Bearer';
-export const endpoint = 'https://tim-gia-su.herokuapp.com/graphql/';
+export const endpoint = 'http://ec2-18-141-198-53.ap-southeast-1.compute.amazonaws.com:8000/graphql/';
 export const server_name_chat_app = 'https://tim-gia-su-chat.herokuapp.com';
 export const endpoint_chat_app = 'https://tim-gia-su-chat.herokuapp.com/graphql';
 export const notification_socket = "wss://tim-gia-su.herokuapp.com/ws/notification/";
