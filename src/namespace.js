08ed166523e901ee109d2734ba7e47bfb2cb85eb
hieucaohd <hieucaohd@gmail.com>
2021-10-17 09:48:39 +0700

    thay doi server

--- src/namespace.js
+++ src/namespace.js
@@ -1,7 +1,9 @@
 const server_main = 'ec2-54-179-117-11.ap-southeast-1.compute.amazonaws.com'
 
-export const server_name = `http://${server_main}`;
-export const endpoint = `http://${server_main}/graphql/`;
+const server_main_api = `${server_main}/api`
+
+export const server_name = `http://${server_main_api}`;
+export const endpoint = `http://${server_main_api}/graphql/`;
 export const server_websocket = `ws://${server_main}/ws`;
 
 export const notification_socket = `${server_websocket}/notification/`;
