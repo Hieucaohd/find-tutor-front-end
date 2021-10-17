b0eb8e668b4731e0c16b629f7607ce7598b858c2
hieucaohd <hieucaohd@gmail.com>
2021-10-17 14:46:57 +0700

    thay doi server name

--- src/namespace.js
+++ src/namespace.js
@@ -1,15 +1,17 @@
-const server_main = 'ec2-54-179-117-11.ap-southeast-1.compute.amazonaws.com'
+const server_main = 'findtutorapp.website'
 
-const server_main_api = `${server_main}/api`
+const server_django_api = `${server_main}/api`
+const server_nodejs_api = `${server_main}/api-chat`
 
-export const server_name = `http://${server_main_api}`;
-export const endpoint = `http://${server_main_api}/graphql/`;
-export const server_websocket = `ws://${server_main}/ws`;
+export const server_name = `https://${server_django_api}`;
+export const endpoint = `https://${server_django_api}/graphql/`;
+export const server_websocket = `wss://${server_django_api}/ws`;
 
 export const notification_socket = `${server_websocket}/notification/`;
 export const room_socket = `${server_websocket}/room/`;
 
-export const server_name_chat_app = 'https://tim-gia-su-chat.herokuapp.com';
-export const endpoint_chat_app = 'https://tim-gia-su-chat.herokuapp.com/graphql';
+
+export const server_name_chat_app = `https://${server_nodejs_api}`;
+export const endpoint_chat_app = `https://${server_nodejs_api}/graphql`;
 
 export const token_prefix = 'Bearer';
