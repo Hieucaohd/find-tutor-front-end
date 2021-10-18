68be4a43e4c06d30a58729fb85e5992757b5b884
hiepnguyen223 <hiepnguyenn223@gmail.com>
2021-10-18 16:33:35 +0700

    fix image in parent room

--- src/features/ParentRoom/components/ParentRoomMain/components/RoomInfo.jsx
+++ src/features/ParentRoom/components/ParentRoomMain/components/RoomInfo.jsx
@@ -64,7 +64,7 @@ function RoomInfo( {room, applyList, userId, addToApplyList, typeParent, teachin
         <div className={classes.root}>
             <div className={classes.room}>
             <div className={classes.header}>
-                <img src={subject[room?.subject] || subject["Mặc Định"]} alt="mon hoc"/>
+                <img src={subject[room.subject.trim()].default || subject["Mặc Định"].default} alt="mon hoc"/>
                 <div className={classes.info}>
                     <h3>{room?.subject} {room?.lop}</h3>
                     <h4>{room?.address}</h4>
