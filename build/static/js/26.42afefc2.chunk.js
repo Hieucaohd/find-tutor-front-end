c256b50d6627335be3ceaeeb679ebef4f246a1ee
hiepnguyen223 <hiepnguyenn223@gmail.com>
2021-11-03 16:50:39 +0700

    change cookie to local storage

--- build/static/js/26.42afefc2.chunk.js
+++
@@ -1,2 +0,0 @@
-(this.webpackJsonpbaseapp=this.webpackJsonpbaseapp||[]).push([[26],{176:function(t,s,e){},185:function(t,s,e){"use strict";e.r(s);e(1);var a=e(7),c=(e(176),e(2));s.default=function(t){var s=t.roomAddress;return Object(c.jsx)("div",{className:"button",children:Object(c.jsx)(a.b,{to:s,className:"button__detail",children:"Xem chi ti\u1ebft"})})}}}]);
-//# sourceMappingURL=26.42afefc2.chunk.js.map
