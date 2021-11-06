ad89d777fde54b8596252a00e3f8c2ad32b1aac9
hiepnguyen223 <hiepnguyenn223@gmail.com>
2021-11-06 16:23:37 +0700

    add axios

--- build/static/js/26.cd742b00.chunk.js
+++
@@ -1,2 +0,0 @@
-(this.webpackJsonpbaseapp=this.webpackJsonpbaseapp||[]).push([[26],{175:function(t,s,e){},183:function(t,s,e){"use strict";e.r(s);e(1);var a=e(7),c=(e(175),e(2));s.default=function(t){var s=t.roomAddress;return Object(c.jsx)("div",{className:"button",children:Object(c.jsx)(a.b,{to:s,className:"button__detail",children:"Xem chi ti\u1ebft"})})}}}]);
-//# sourceMappingURL=26.cd742b00.chunk.js.map
